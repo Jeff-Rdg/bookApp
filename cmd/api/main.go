4450217a40ca5629c47f47964485c9e3f98e86e9
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2024-01-01 12:22:58 -0300

    create method main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"BookApp/configs"
+	"BookApp/internal/routes"
+	"net/http"
+)
 
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := configs.LoadDatabase()
+	if err != nil {
+		panic(err)
+	}
+
+	r := routes.LoadRoutes(db)
+
+	http.ListenAndServe(":8000", r)
 }
