a1c0847a96f9fed4f3cff3d4ed462b0a1cc3ca51
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2024-01-01 12:22:40 -0300

    create routes.go

---
+++ internal/routes/routes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"BookApp/author"
+	"BookApp/internal/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"gorm.io/gorm"
+)
+
+func LoadRoutes(db *gorm.DB) *chi.Mux {
+	authorService := author.NewService(db)
+	authorHandler := handlers.NewAuthorHandler(authorService)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/author", func(r chi.Router) {
+		r.Get("/{id}", authorHandler.FindById)
+		r.Post("/upload_csv", authorHandler.UploadCsv)
+	})
+
+	return r
+}
