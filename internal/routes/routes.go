2d3cf6fba4f297373b614079d2f66f2533a86f65
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2024-01-09 22:16:38 -0300

    add routes to book

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"BookApp/author"
+	"BookApp/book"
 	"BookApp/internal/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -12,6 +13,9 @@ func LoadRoutes(db *gorm.DB) *chi.Mux {
 	authorService := author.NewService(db)
 	authorHandler := handlers.NewAuthorHandler(authorService)
 
+	bookService := book.NewService(db)
+	bookHandler := handlers.NewBookHandler(bookService)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
@@ -20,6 +24,10 @@ func LoadRoutes(db *gorm.DB) *chi.Mux {
 		r.Get("/{id}", authorHandler.FindById)
 		r.Get("/list", authorHandler.List)
 		r.Post("/upload_csv", authorHandler.UploadCsv)
+	})
+
+	r.Route("/book", func(r chi.Router) {
+		r.Get("/{id}", bookHandler.FindById)
 	})
 
 	return r
